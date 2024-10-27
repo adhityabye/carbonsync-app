fa973bd52db41736ef8da83a1576aea630685d5b
hisazakura <luthfanarrayyan@gmail.com>
2024-10-27 15:50:03 +0700

    rework home ui

--- app/src/main/java/io/apaaja/carbonsync/ui/home/HomeFragment.kt
+++ app/src/main/java/io/apaaja/carbonsync/ui/home/HomeFragment.kt
@@ -8,14 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.card.MaterialCardView
 import io.apaaja.carbonsync.MainActivity
 import io.apaaja.carbonsync.R
 import io.apaaja.carbonsync.databinding.FragmentHomeBinding
+import io.apaaja.carbonsync.ui.dashboard.DashboardViewModel
 import io.apaaja.carbonsync.viewmodel.CarbonDataViewModel
 
 class HomeFragment : Fragment() {
 
     private lateinit var carbonDataViewModel: CarbonDataViewModel
+    private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -34,12 +37,12 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         carbonDataViewModel = ViewModelProvider(requireActivity())[CarbonDataViewModel::class.java]
+        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
         setupObservers()
 
-        // make it clickable
-        binding.layoutCarbonViewTop.setOnClickListener(getCarbonReductionLayoutOnClickListener())
-        binding.communityButton.setOnClickListener(View.OnClickListener { (activity as MainActivity).openCommunityActivity(requireView()); })
+        val carbonReductionCard: MaterialCardView = view.findViewById(R.id.card_carbon_reduction)
+        carbonReductionCard.setOnClickListener(getCarbonReductionLayoutOnClickListener())
     }
 
     override fun onResume() {
@@ -54,7 +57,15 @@ class HomeFragment : Fragment() {
 
     private fun setupObservers() {
         carbonDataViewModel.currentDateCarbonData.observe(viewLifecycleOwner) {
-            carbonData -> binding.textviewCarbonViewCenter.text = getString(R.string.home_carbon_view_center_format, carbonData.total())
+            carbonData -> binding.textviewCarbonView.text = getString(R.string.home_carbon_view_format, carbonData.total())
+        }
+        homeViewModel.screenTime.observe(viewLifecycleOwner) {
+            screenTime -> binding.textviewScreenTimeView.text =
+                getString(R.string.home_activity_screen_time_view_format, screenTime.toHours(), screenTime.toMinutes() % 60)
+        }
+        homeViewModel.achievements.observe(viewLifecycleOwner) {
+            achievements -> binding.textviewAchievementsView.text =
+                getString(R.string.home_activity_achievements_view_format, achievements.first, achievements.second)
         }
     }
 
--- app/src/main/java/io/apaaja/carbonsync/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/apaaja/carbonsync/ui/home/HomeViewModel.kt
@@ -3,14 +3,16 @@ package io.apaaja.carbonsync.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.Duration
 
 class HomeViewModel : ViewModel() {
-    private val _carbonReduction = MutableLiveData<Float>().apply {
-        value = 0.0f
+    private val _screenTime = MutableLiveData<Duration>().apply {
+        value = Duration.ZERO
     }
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _achievements = MutableLiveData<Pair<Int, Int>>().apply {
+        value = Pair(0, 0)
     }
-    val text: LiveData<String> = _text
-    val carbonReduction: LiveData<Float> = _carbonReduction
+
+    val screenTime: LiveData<Duration> = _screenTime
+    val achievements: LiveData<Pair<Int, Int>> = _achievements
 }
