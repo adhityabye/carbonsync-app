839cf04d873c538b7493de69eccec34ecb078bd2
hisazakura <luthfanarrayyan@gmail.com>
2024-11-29 08:07:06 +0700

    implement acheviement functionality

--- app/src/main/java/io/apaaja/carbonsync/ui/activity/AchievementFragment.kt
+++ app/src/main/java/io/apaaja/carbonsync/ui/activity/AchievementFragment.kt
@@ -6,7 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.MediatorLiveData
+import io.apaaja.carbonsync.MainActivity
 import io.apaaja.carbonsync.R
+import io.apaaja.carbonsync.utils.formatter.IntegerNumberFormatter
+import io.apaaja.carbonsync.viewmodel.CarbonDataViewModel
+import io.apaaja.carbonsync.viewmodel.CarbonDataViewModelFactory
 
 class AchievementFragment : Fragment() {
 
@@ -15,6 +22,7 @@ class AchievementFragment : Fragment() {
     }
 
     private lateinit var viewModel: AchievementViewModel
+    private lateinit var carbonDataViewModel: CarbonDataViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +31,54 @@ class AchievementFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_achievement, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AchievementViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[AchievementViewModel::class.java]
+        carbonDataViewModel = ViewModelProvider(
+            requireActivity(),
+            CarbonDataViewModelFactory((requireActivity() as MainActivity).getCarbonActivitiesRepository())
+        )[CarbonDataViewModel::class.java]
+
+        val totalValue: TextView = view.findViewById(R.id.textview_achievement_total_value)
+        val totalProgress: ProgressBar = view.findViewById(R.id.progressbar_achievement_total)
+        val highestValue: TextView = view.findViewById(R.id.textview_achievement_highest_value)
+        val highestProgress: ProgressBar = view.findViewById(R.id.progressbar_achievement_highest_progress)
+
+        val combinedTotalData = MediatorLiveData<Pair<Int, Int?>>()
+
+        combinedTotalData.addSource(viewModel.totalTarget) { totalTarget ->
+            combinedTotalData.value = Pair(totalTarget ?: 0, carbonDataViewModel.getTotal())
+        }
+
+        combinedTotalData.addSource(carbonDataViewModel.totalData) { totalData ->
+            combinedTotalData.value = Pair(viewModel.totalTarget.value ?: 0, totalData)
+        }
+
+        combinedTotalData.observe(viewLifecycleOwner) { (totalTarget, totalData) ->
+            totalValue.text = String.format(getString(R.string.achievements_progress_format),
+                IntegerNumberFormatter.condense(totalData ?: 0),
+                IntegerNumberFormatter.condense(totalTarget ?: 0))
+            totalProgress.progress = if (totalTarget > 0) (totalData ?: 0) * 100 / totalTarget else 0
+        }
+
+        val combinedHighestData = MediatorLiveData<Pair<Int, Int?>>()
+
+        combinedHighestData.addSource(viewModel.highestTarget) { totalTarget ->
+            combinedHighestData.value = Pair(totalTarget ?: 0, carbonDataViewModel.highestDailyReduction.value)
+        }
+
+        combinedHighestData.addSource(carbonDataViewModel.highestDailyReduction) { totalData ->
+            combinedHighestData.value = Pair(viewModel.highestTarget.value ?: 0, totalData)
+        }
+
+        combinedHighestData.observe(viewLifecycleOwner) { (highestTarget, highestData) ->
+            highestValue.text = String.format(getString(R.string.achievements_progress_format),
+                IntegerNumberFormatter.condense(highestData ?: 0),
+                IntegerNumberFormatter.condense(highestTarget ?: 0))
+            highestProgress.progress = if (highestData ?: 0 > 0) (highestData ?: 0) * 100 / highestTarget else 0
+        }
     }
+
+
 
 }
--- app/src/main/java/io/apaaja/carbonsync/ui/activity/AchievementViewModel.kt
+++ app/src/main/java/io/apaaja/carbonsync/ui/activity/AchievementViewModel.kt
@@ -1,7 +1,13 @@
 package io.apaaja.carbonsync.ui.activity
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AchievementViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _totalTarget = MutableLiveData<Int>(100000)
+    val totalTarget: LiveData<Int> = _totalTarget
+
+    private val _highestTarget = MutableLiveData<Int>(10000)
+    val highestTarget: LiveData<Int> = _highestTarget
 }
